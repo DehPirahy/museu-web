b6610be1e426e94ff474616cbd9d215925b1ed8a
ThiagoAndrad <thpandrade@gmail.com>
2016-06-15 19:41:12 -0300

    [melhorando template]

--- src/br/com/fatec/museu/bean/LoginBean.java
+++ src/br/com/fatec/museu/bean/LoginBean.java
@@ -19,15 +19,12 @@ public class LoginBean {
 	}
 	
 	public String efetuaLogin() {
-		System.out.println("fazendo login do usuario " + this.usuario.getEmail());
-		
 		FacesContext context = FacesContext.getCurrentInstance();
 		boolean existe = new UsuarioDao().existe(this.usuario);
 		if(existe ) {
 			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
 			return "obra?faces-redirect=true";
 		}
-		
 		context.getExternalContext().getFlash().setKeepMessages(true);
 		context.addMessage(null, new FacesMessage("Usuário não encontrado"));
 		
